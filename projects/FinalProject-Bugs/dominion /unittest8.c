b2c9a00dca0bc3379edd7592c08e53bb195a4c8f
DafeiDu <38805258+DafeiDu@users.noreply.github.com>
2019-12-10 14:12:21 -0800

    Update unittest8.c

--- projects/FinalProject-Bugs/dominion /unittest8.c
+++ projects/FinalProject-Bugs/dominion /unittest8.c
@@ -5,10 +5,10 @@
 
 #include "dominion.h"
 #include "dominion_helpers.h"
+#include "rngs.h"
 #include <string.h>
 #include <stdio.h>
 #include <assert.h>
-#include <rngs.h>
 #include <stdlib.h>
 
 int main(){
