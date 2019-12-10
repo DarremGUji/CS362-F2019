166a59eb966e4377e42e7ff1c4a796fcbf287039
DafeiDu <38805258+DafeiDu@users.noreply.github.com>
2019-12-10 14:11:37 -0800

    Update unittest5.c

--- projects/FinalProject-Bugs/dominion /unittest5.c
+++ projects/FinalProject-Bugs/dominion /unittest5.c
@@ -5,10 +5,10 @@
 
 #include "dominion.h"
 #include "dominion_helpers.h"
+#include "rngs.h"
 #include <string.h>
 #include <stdio.h>
 #include <assert.h>
-#include <rngs.h>
 #include <stdlib.h>
 
 int testScoreFor1(struct gameState G){
